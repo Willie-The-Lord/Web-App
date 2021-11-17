cd6332e5fe9cded95df992dbe64539a4373f2d86
Willie Hung <Willie.Hung@lumentum.com>
2021-11-17 11:55:49 +0800

    initial commit

---
+++ Models/Joke.cs
@@ -0,0 +1,17 @@
+﻿namespace JokesApp.Models
+{
+    public class Joke
+    {
+
+        public int ID { get; set; }
+
+        public string JokeQuestion { get; set; }
+
+        public string JokeAnswer { get; set; }
+
+        public Joke()
+        {
+
+        }
+    }
+}
---
+++ dbo.Joke.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Joke] (
+    [ID]           INT            IDENTITY (1, 1) NOT NULL,
+    [JokeQuestion] NVARCHAR (MAX) NOT NULL,
+    [JokeAnswer]   NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_Joke] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
